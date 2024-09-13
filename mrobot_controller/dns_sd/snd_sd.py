68abb8ea41c4ca782987e95ef5a5db69941e254a
Amnon Paz <pazamnon@gmail.com>
2024-09-13 17:47:52 +0300

    DNS SD: Publish service on all local IPs

--- mrobot_controller/dns_sd/snd_sd.py
+++ mrobot_controller/dns_sd/snd_sd.py
@@ -1,4 +1,5 @@
 import socket
+import netifaces
 from zeroconf import ServiceInfo, Zeroconf
 import logging
 
@@ -11,20 +12,23 @@ class ServicePublisher:
         self.service_name = f'_{service_name}._websocket._tcp.local.'
         self.port = port
 
-        local_ip = socket.gethostbyname(socket.gethostname())
+        self.local_ips = [netifaces.ifaddresses(iface)[netifaces.AF_INET][0]['addr'] for iface in netifaces.interfaces() if netifaces.AF_INET in netifaces.ifaddresses(iface)]
 
         self.info = ServiceInfo(
             '_websocket._tcp.local.',
             name=self.service_name,
-            addresses=[socket.inet_aton(local_ip)],
+            addresses=self.local_ips, # socket.inet_aton(self.local_ips),
             port=port,
             properties={'path': '/ws'},
             server="websocket-server.local."
         )
 
+    def get_ips(self):
+        return self.local_ips
+
     def publish(self):
         self.zeroconf.register_service(self.info)
-        self.logger.info(f"WebSocket service '{self.service_name}' published on port {self.port}")
+        self.logger.info(f"WebSocket service '{self.service_name}' published on {self.info.parsed_addresses()}")
 
     def unpublish(self):
         self.zeroconf.unregister_service(self.info)
